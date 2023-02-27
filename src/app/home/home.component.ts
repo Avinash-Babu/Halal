b55c919f5cb5c3dcedac73f5edf204144a3f2784
Avinash-Babu <avinash.babu@softsuave.com>
2023-02-27 20:04:49 +0530

    latest recipes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -30,9 +30,60 @@ export class HomeComponent implements OnInit {
       desc: 'Habitant accumsan suscipit sodales phasellus nulla elit placerat sapien quisque gravisa tincidunt'
     },
     {
-      imgURL: '/assets/images/Layer_15.png',
+      imgURL: '/assets/images/Layer_18.png',
       type: 'MAIN DISHES',
       title: 'Jamie\'s Baked Brie',
+      desc: 'Habitant accumsan suscipit sodales phasellus nulla elit placerat sapien quisque gravisa tincidunt'
+    },
+  ]
+
+  latestRecipes = [
+    {
+      imgURL: '/assets/images/Layer_21.png',
+      type: 'MAIN DISHES',
+      title: 'Quick Chicken Picata',
+      desc: 'Habitant accumsan suscipit sodales phasellus nulla elit placerat sapien quisque gravisa tincidunt'
+    },
+    {
+      imgURL: '/assets/images/Layer_22.png',
+      type: 'MAIN DISHES',
+      title: 'Kung Pao Chicken',
+      desc: 'Habitant accumsan suscipit sodales phasellus nulla elit placerat sapien quisque gravisa tincidunt'
+    },
+    {
+      imgURL: '/assets/images/Layer_26.png',
+      type: 'MAIN DISHES',
+      title: 'Honey-Garlic Slow Cooker Chicken Thighs',
+      desc: 'Habitant accumsan suscipit sodales phasellus nulla elit placerat sapien quisque gravisa tincidunt'
+    },
+    {
+      imgURL: '/assets/images/Layer_27.png',
+      type: 'MAIN DISHES',
+      title: 'Jamie\'s Baked Brie',
+      desc: 'Habitant accumsan suscipit sodales phasellus nulla elit placerat sapien quisque gravisa tincidunt'
+    },
+    {
+      imgURL: '/assets/images/Layer_21.png',
+      type: 'APPETIZERS AND SNACKS',
+      title: 'Baked Brie with Caramelized Pears',
+      desc: 'Habitant accumsan suscipit sodales phasellus nulla elit placerat sapien quisque gravisa tincidunt'
+    },
+    {
+      imgURL: '/assets/images/Layer_22.png',
+      type: 'APPETIZERS AND SNACKS',
+      title: 'Baked Brie in Puff Pastry',
+      desc: 'Habitant accumsan suscipit sodales phasellus nulla elit placerat sapien quisque gravisa tincidunt'
+    },
+    {
+      imgURL: '/assets/images/Layer_26.png',
+      type: 'DRINK RECIPES',
+      title: 'Peanut Butter Banana Smoothie',
+      desc: 'Habitant accumsan suscipit sodales phasellus nulla elit placerat sapien quisque gravisa tincidunt'
+    },
+    {
+      imgURL: '/assets/images/Layer_27.png',
+      type: 'DRINK RECIPES',
+      title: 'The Real Mojito',
       desc: 'Habitant accumsan suscipit sodales phasellus nulla elit placerat sapien quisque gravisa tincidunt'
     },
   ]
