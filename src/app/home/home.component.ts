01a7e0857e0534ccffc50cfba1ac6aa87113d711
DESKTOP-3IKGCEH\Softsuave <pandurangan.sermaraj@softsuave.com>
2023-02-27 14:47:17 +0530

    initial setup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,7 +10,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() {
+   }
 
   ngOnInit(): void {
   }
